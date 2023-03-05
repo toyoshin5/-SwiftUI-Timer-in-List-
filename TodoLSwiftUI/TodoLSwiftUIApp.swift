c51fd389c25758f63343db52d327efcfc127f1e6
Shingo TOYODA <toyodasinngo@gmail.com>
2023-03-05 14:05:17 +0900

    Initial Commit

---
+++ TodoLSwiftUI/TodoLSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoLSwiftUIApp.swift
+//  TodoLSwiftUI
+//
+//  Created by Shin Toyo on 2023/03/05.
+//
+
+import SwiftUI
+
+@main
+struct TodoLSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
